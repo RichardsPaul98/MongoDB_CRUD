88bafa33a1a5e190a56c16e751cfbbb0cdbb4696
Richards Paul <33003042+RichardsPaul98@users.noreply.github.com>
2021-09-13 01:56:50 +0530

    updated with the special queries
    Endpoint the four special case scenario is built

--- flask_api_crud.py
+++ flask_api_crud.py
@@ -40,6 +40,40 @@ class CrudAPI:  # MongoDB Model for ToDo CRUD Implementation
         output = {'Status': 'Successfully Deleted' if response.deleted_count > 0 else "Document not found."}
         return output
 
+    def count_discounted_products_data(self):    # count_discounted_products_data
+        aggregation = self.data['aggregation']
+        documents = self.collection.aggregate(aggregation)
+        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        return output
+    
+    def list_unique_brands_data(self):    # list_unique_brands
+        aggregation = self.data['aggregation']
+        documents = self.collection.aggregate(aggregation)
+        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        return output
+    
+    def count_high_offer_price_data(self):    # count_high_offer_price
+        greaterthan = self.data['Filter']['greaterthan']
+        field_name = self.data['Filter']['field_name']
+        # stri = f"{greaterthan}"
+        filter = f"""{{ {field_name}: {{ $gt: {greaterthan} }} }}"""
+        documents = self.collection.find(filter)
+        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        return output
+
+    def count_high_discount_data(self):    # count_high_discount
+        greaterthan = self.data['percentage']
+        documents = self.collection.find()
+        output = [
+            {
+                item: data[item] for item in data if item != '_id' and 
+                (((data['regular_price_value'] - data['offer_price_value ']/data['regular_price_value'])*100)>greaterthan)
+            } 
+            for data in documents
+        ]
+
+        return output
+
 # Achieving CRUD through API - '/crud_mongodb'
 @app.route('/crud_mongodb', methods=['GET'])     # Read MongoDB Document, through API and METHOD - GET
 def read_data():
@@ -80,7 +114,6 @@ def update():
     return Response(response=json.dumps(response), status=200,
                     mimetype='application/json')
 
-
 @app.route('/crud_mongodb', methods=['DELETE'])   # Delete MongoDB Document, through API and METHOD - DELETE
 def delete():
     data = request.json
@@ -92,6 +125,50 @@ def delete():
                         status=400, mimetype='application/json')
     delete_obj = CrudAPI(data)
     response = delete_obj.delete_data(data)
+    return Response(response=json.dumps(response), status=200,
+                    mimetype='application/json')
+
+@app.route('/count_discounted_products', methods=['GET'])  # How many products have a discount on them? and METHOD - GET
+def count_discounted_products():
+    data = request.json
+    if data is None or data == {}:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400, mimetype='application/json')
+    read_obj = CrudAPI(data)
+    response = read_obj.count_discounted_products_data()
+    return Response(response=json.dumps(response), status=200,
+                    mimetype='application/json')
+
+@app.route('/list_unique_brands', methods=['GET'])  # How many unique brands are present in the collection? and METHOD - GET
+def list_unique_brands():
+    data = request.json
+    if data is None or data == {}:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400, mimetype='application/json')
+    read_obj = CrudAPI(data)
+    response = read_obj.list_unique_brands_data()
+    return Response(response=json.dumps(response), status=200,
+                    mimetype='application/json')
+
+@app.route('/count_high_offer_price', methods=['GET'])  # How many products have offer price greater than 300? and METHOD - GET
+def count_high_offer_price():
+    data = request.json
+    if data is None or data == {}:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400, mimetype='application/json')
+    read_obj = CrudAPI(data)
+    response = read_obj.count_high_offer_price_data()
+    return Response(response=json.dumps(response), status=200,
+                    mimetype='application/json')
+
+@app.route('/count_high_discount', methods=['GET'])  # How many products have discount % greater than 30%? and METHOD - GET
+def count_high_discount():
+    data = request.json
+    if data is None or data == {}:
+        return Response(response=json.dumps({"Error": "Please provide connection information"}),
+                        status=400, mimetype='application/json')
+    read_obj = CrudAPI(data)
+    response = read_obj.count_high_offer_price_data()
     return Response(response=json.dumps(response), status=200,
                     mimetype='application/json')
 
